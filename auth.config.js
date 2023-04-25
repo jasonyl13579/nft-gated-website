a5a1f95224307da9c87160e9fff592195c8d4f0c
Jason <jasonyl13579@gmail.com>
2023-04-25 13:29:25 -0700

    Add dotenv expand

--- auth.config.js
+++ auth.config.js
@@ -1,8 +1,11 @@
 import { ThirdwebAuth } from "@thirdweb-dev/auth/next";
 import { PrivateKeyWallet } from "@thirdweb-dev/auth/evm";
-import { domainName } from "./const/yourDetails";
+const DOMINE_NAME = process.env.NEXT_PUBLIC_URL;
+if (!DOMINE_NAME) {
+  throw new Error("You need to add an NEXT_PUBLIC_URL environment variable.");
+}
 
 export const { ThirdwebAuthHandler, getUser } = ThirdwebAuth({
-  domain: domainName,
+  domain: DOMINE_NAME,
   wallet: new PrivateKeyWallet(process.env.THIRDWEB_AUTH_PRIVATE_KEY || ""),
 });
--- const/yourDetails.js
+++ const/yourDetails.js
@@ -2,7 +2,7 @@
 export const contractAddress = "0xa0841C85Ba61a7839392ad8b96a714Bea737bE47";
 
 // Replace this with your domain name.
-export const domainName = "127.0.0.1";
+//export const domainName = "127.0.0.1";
 
 // Minimum number of tokens required to access
 export const minimumBalance = 2;
