7356e0c7622041ba097830c5367f59ea29217a16
Jason <jasonyl13579@gmail.com>
2023-04-25 12:38:31 -0700

    add yourDetails

---
+++ const/yourDetails.js
@@ -0,0 +1,13 @@
+// Replace this with your contract address
+export const contractAddress = "0xa0841C85Ba61a7839392ad8b96a714Bea737bE47";
+
+// Replace this with your domain name.
+export const domainName = "127.0.0.1";
+
+// Minimum number of tokens required to access
+export const minimumBalance = 2;
+
+// Token ID for ERC-1155 tokens
+export const erc1155TokenId = 0;
+
+
